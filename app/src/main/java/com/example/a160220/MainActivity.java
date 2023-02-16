a71057859e8aed094d81ca7abd387cd2b80a29b5
mtatiPL <mateusz-biernat@tlen.pl>
2023-02-16 15:04:31 +0100

    zgoda na lokalizacje

--- app/src/main/java/com/example/a160220/MainActivity.java
+++ app/src/main/java/com/example/a160220/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.a160220;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int REQUEST_LOCATION_PERMISSION=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sprawdzLokalizacje();
+            }
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            sprawdzLokalizacje();
+
+
+
+        }
+        else {
+            Toast.makeText(this, "Nie wyrażono zgody na dalsze działanie aplikacji", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+private void sprawdzLokalizacje(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{  Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+        else {
+            Log.d("lokalizacja", "wyrażona zgoda na lokalizację ");
+        }
+
+}
 }
